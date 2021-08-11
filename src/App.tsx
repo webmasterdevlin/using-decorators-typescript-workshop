7995d7310409066e28e3a6fe22ec7a83b42fd427
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-08-11 23:20:16 +0200

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,32 @@
-import React from 'react';
-import './App.css';
-import {sendTS, tryMethodDecorator, tryPropsDecorator} from 'insta-email';
+import React from "react";
+import "./App.css";
+import { sendTS, tryMethodDecorator, tryPropsDecorator } from "insta-email";
 
 class App extends React.Component {
-
   @tryPropsDecorator()
   state = {
-    first:'Devlin',
-    last: 'Duldulao'
+    first: "Devlin",
+    last: "Duldulao",
   };
 
   @tryMethodDecorator()
   sendEmail() {
-    sendTS({mailto:'webmasterdevlin@gmail.com',body:"Hello",subject:"Hey sexy!"});
+    sendTS({
+      mailto: "webmasterdevlin@gmail.com",
+      body: "Hello",
+      subject: "Hey sexy!",
+    });
   }
-
 
   componentDidMount() {
     let me = this.state.first;
-    console.log('me::', me);
+    console.log("me::", me);
     this.sendEmail();
   }
 
-public render() {
-  return (
-    <h1>Hello</h1>
-  );
-}
+  public render() {
+    return <h1>Hello</h1>;
+  }
 }
 
 export default App;
